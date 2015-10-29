ca8643037be6d25c853da170bf9ad7713cac6d93
Alex Palcuie <alex.palcuie@hootsuite.com>
2015-10-29 13:34:01 +0200

    maybe c

---
+++ ts/lab4/script2.m
@@ -0,0 +1,27 @@
+function x =  simVarDiscreta(X, p)
+    v = zeros(1, size(p, 2) + 1);
+    L = length(p) + 1;
+    for i = 2:L
+        v(i) = v(i-1) + p(i-1);
+    end
+
+    u = rand();
+    for i = 2:L
+        if u < v(i)
+            x = X(i-1);
+            break
+        end
+    end
+end
+
+X = 0:10;
+p = binopdf(X, 10, 0.5);
+x = simVarDiscreta(X, p);
+
+N = 1000;
+Y = zeros(1, N);
+for i = 1:N
+    Y(i) = simVarDiscreta(X, p);
+end
+
+hist(Y);
