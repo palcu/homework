975a8ad1132d6371aaaabb275970eb1fa48c1a1e
Alex Palcuie <alex.palcuie@gmail.com>
2016-04-24 15:41:14 +0300

    Update false position

--- cn/tema1/false_position.py
+++ cn/tema1/false_position.py
@@ -3,16 +3,18 @@ from decimal import *
 
 def get_root(interval):
     a, b = interval
+    old_c = 2**1000
     step = 0
     while step < MAX_ITERATIONS:
         step += 1
-        c = a - (b - a) / (f(b) - f(a)) * f(a)
-        if abs(f(c)) < EPSILON:
+        c = (a*f(b)-b*f(a)) / (f(b) - f(a))
+        if abs(f(c)) < EPSILON or abs(c-old_c) < EPSILON:
             return c
         if same_sign(f(c), f(a)):
             a = c
         else:
             b = c
+        old_c = c
 
 def correct_interval(interval):
     return not same_sign(f(interval[0]), f(interval[1]))
@@ -20,5 +22,3 @@ def correct_interval(interval):
 for interval in INTERVALS:
     if correct_interval(interval):
         print(Decimal(get_root(interval)))
-
-# Problema: nu imi converge pentru toate intervalele
